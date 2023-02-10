5c650ca9e9d2fef2535ea55ed1d9034d16bc78d1
fycn <fannychen623@gmail.com>
2023-02-10 00:24:12 -0500

    Added view employee by manager and view employee by department functionality

--- server.js
+++ server.js
@@ -32,7 +32,12 @@ app.use((req, res) => {
 
 module.exports = db;
 
-// db.query(`Select * from departments`, (err, results) => {
+// db.query(`SELECT employees.id, employees.first_name, employees.last_name, roles.title, roles.salary, department_id, departments.name FROM employees LEFT JOIN roles ON employees.title_id = roles.id LEFT JOIN departments ON roles.department_id = departments.id`, (err, results) => {
+//   console.table(results);
+//   process.exit(0);
+// });
+
+// db.query(`Select * from employees`, (err, results) => {
 //   console.table(results);
 //   process.exit(0);
 // });
